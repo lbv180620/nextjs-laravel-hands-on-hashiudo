50ea272a916d63408e44847db0d78ae1c6e4ed1c
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2022-10-02 02:13:33 +0900

    save

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -27,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
 
-        if (config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
+        // if (config('app.env') === 'production') {
+        //     \URL::forceScheme('https');
+        // }
     }
 }
