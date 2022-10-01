6e231abcdb84d35e07ad6fd79bec18e186639708
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2022-10-01 13:44:03 +0900

    save

--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -13,10 +13,10 @@ import Axios from "axios";
 const axios = Axios.create({
   // baseURL: "http://localhost:8080",
   baseURL: "https://nextjs-laravel-hands-on.herokuapp.com/",
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   'X-Requested-With': 'XMLHttpRequest',
-  // },
+  headers: {
+    "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+  },
   withCredentials: true,
 });
 
