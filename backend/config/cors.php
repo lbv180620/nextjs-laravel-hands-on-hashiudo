dc48bc772ea21b4a21ed464c04121798876d9773
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2022-10-27 08:06:05 +0900

    save

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['https://nextjs-laravel-hands-on-hashiudo.vercel.app'],
+    'allowed_origins' => ['https://nextjs-laravel-hands-on-hashiudo.vercel.app', 'https://nextjs-laravel-hands-on-front.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
