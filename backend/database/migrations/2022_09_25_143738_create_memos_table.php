ebed0bed8eb7abec2e819f55e3a8f2cb15327fd8
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2022-11-01 11:41:45 +0900

    save

--- backend/database/migrations/2022_09_25_143738_create_memos_table.php
+++ backend/database/migrations/2022_09_25_143738_create_memos_table.php
@@ -36,41 +36,41 @@ class CreateMemosTable extends Migration
         });
 
         // 関数の定義
-        DB::statement("
-                CREATE FUNCTION refresh_updated_at_step1() RETURNS trigger AS
-                $$
-                BEGIN
-                    IF NEW.updated_at = OLD.updated_at THEN
-                        NEW.updated_at := NULL;
-                    END IF;
-                    RETURN NEW;
-                END;
-                $$ LANGUAGE plpgsql;
-            ");
+        // DB::statement("
+        //         CREATE FUNCTION refresh_updated_at_step1() RETURNS trigger AS
+        //         $$
+        //         BEGIN
+        //             IF NEW.updated_at = OLD.updated_at THEN
+        //                 NEW.updated_at := NULL;
+        //             END IF;
+        //             RETURN NEW;
+        //         END;
+        //         $$ LANGUAGE plpgsql;
+        //     ");
 
-        DB::statement("
-                CREATE FUNCTION refresh_updated_at_step2() RETURNS trigger AS
-                $$
-                BEGIN
-                    IF NEW.updated_at IS NULL THEN
-                        NEW.updated_at := OLD.updated_at;
-                    END IF;
-                    RETURN NEW;
-                END;
-                $$ LANGUAGE plpgsql;
-            ");
+        // DB::statement("
+        //         CREATE FUNCTION refresh_updated_at_step2() RETURNS trigger AS
+        //         $$
+        //         BEGIN
+        //             IF NEW.updated_at IS NULL THEN
+        //                 NEW.updated_at := OLD.updated_at;
+        //             END IF;
+        //             RETURN NEW;
+        //         END;
+        //         $$ LANGUAGE plpgsql;
+        //     ");
 
-        DB::statement("
-                CREATE FUNCTION refresh_updated_at_step3() RETURNS trigger AS
-                $$
-                BEGIN
-                    IF NEW.updated_at IS NULL THEN
-                        NEW.updated_at := CURRENT_TIMESTAMP;
-                    END IF;
-                    RETURN NEW;
-                END;
-                $$ LANGUAGE plpgsql;
-            ");
+        // DB::statement("
+        //         CREATE FUNCTION refresh_updated_at_step3() RETURNS trigger AS
+        //         $$
+        //         BEGIN
+        //             IF NEW.updated_at IS NULL THEN
+        //                 NEW.updated_at := CURRENT_TIMESTAMP;
+        //             END IF;
+        //             RETURN NEW;
+        //         END;
+        //         $$ LANGUAGE plpgsql;
+        //     ");
 
         // トリガーの定義
         DB::statement("
