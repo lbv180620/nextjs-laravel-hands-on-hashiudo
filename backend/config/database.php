51e09d577079fab17337e870b563fbb700327d3b
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2022-11-05 12:51:19 +0900

    save

--- backend/config/database.php
+++ backend/config/database.php
@@ -173,7 +173,7 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
+            'port' => (int) env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
 
@@ -181,7 +181,7 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
+            'port' => (int) env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
