83a325cb3deb0cb0e9eb7c58a4d745a33f15712a
kazuaki <0chlbv7420t18063263g7t20@gmail.com>
2023-06-13 13:50:24 +0900

    save

--- backend/config/services.php
+++ backend/config/services.php
@@ -32,7 +32,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8080/login/google/callback',
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
 
 ];
